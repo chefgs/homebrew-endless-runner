17c6a1f598452e3f280c7731389c43efb9cb68ef
Saravanan Gnanaguru <g.gsaravanan@gmail.com>
2025-01-14 03:28:48 +0530

    updated tap

--- Formula/endless-runner.rb
+++ Formula/endless-runner.rb
@@ -1,9 +1,8 @@
 class EndlessRunner < Formula
     desc "Endless Runner Game"
     homepage "https://github.com/chefgs/endless-runner"
-    url "https://github.com/chefgs/endless-runner/releases/download/release-v1.0/endless-runner.tar.gz"
+    url "https://github.com/chefgs/endless-runner/archive/refs/tags/release-v1.1.tar.gz"
     sha256 "504952f1d8a96893235a27336d6bf3dcab3728baecb39981d581511d3e3620d7"
-    depends_on "python@3.9"
     
     def install
         bin.install "endless-runner-stickman.py"
