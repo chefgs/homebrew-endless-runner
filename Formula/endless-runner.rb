94e2e91c30eaa5d0900f1cbf8e4a0c606f24634f
Saravanan Gnanaguru <g.gsaravanan@gmail.com>
2025-01-14 02:54:29 +0530

    added tap brew

---
+++ Formula/endless-runner.rb
@@ -0,0 +1,15 @@
+class EndlessRunner < Formula
+    desc "Endless Runner Game"
+    homepage "https://github.com/chefgs/endless-runner"
+    url "https://github.com/chefgs/endless-runner/releases/download/release-v1.0/endless-runner.tar.gz"
+    sha256 "504952f1d8a96893235a27336d6bf3dcab3728baecb39981d581511d3e3620d7"
+    depends_on "python@3.9"
+    depends_on "pygame" 
+    def install
+        bin.install "endless-runner-stickman.py"
+    end
+    
+    test do
+        system "#{bin}/endless-runner-stickman.py", "--version"
+    end
+end
---
+++ README.md
@@ -0,0 +1,18 @@
+# Chefgs Endless-runner
+
+## How do I install these formulae?
+
+`brew install chefgs/endless-runner/<formula>`
+
+Or `brew tap chefgs/endless-runner` and then `brew install <formula>`.
+
+Or, in a [`brew bundle`](https://github.com/Homebrew/homebrew-bundle) `Brewfile`:
+
+```ruby
+tap "chefgs/endless-runner"
+brew "<formula>"
+```
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
