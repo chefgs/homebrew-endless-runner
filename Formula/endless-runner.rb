f62bbcbcffd49326b4198d325789f525313ea8b8
Saravanan Gnanaguru <g.gsaravanan@gmail.com>
2025-01-17 20:04:47 +0530

    added brew formula

--- Formula/endless-runner.rb
+++ Formula/endless-runner.rb
@@ -3,7 +3,7 @@ class EndlessRunner < Formula
     desc "Endless Runner Game"
     homepage "https://github.com/chefgs/endless-runner"
     url "https://github.com/chefgs/endless-runner/releases/download/release-v2.0.0/endless-runner.tz"
-    sha256 "5ebaf7a09c0aef2da33933a61e53c4a51356650ab459735bf833485ab8863545"
+    sha256 "c4acb9b713287f28d5a162854a3bd4607dd930c79e53a8cbff77e36f45017582"
     depends_on "python@3.9"
     def install
         virtualenv_install_with_resources
